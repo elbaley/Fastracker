686ce42a704597a2fda162d7b0686e791bcee389
elbaley <lebafurkan@outlook.com>
2023-07-14 14:43:34 +0300

    Countdown test

--- src/components/Countdown.test.tsx
+++ src/components/Countdown.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import {it, describe} from '@jest/globals';
 
-import {render, screen, fireEvent} from '@testing-library/react-native';
+import {render, screen} from '@testing-library/react-native';
 import Countdown from './Countdown';
 import {Provider} from 'react-redux';
 import store from '../app/store';
@@ -16,14 +16,18 @@ describe('Countdown', () => {
       </Provider>,
     );
   });
-  it('shows correct remaining time', async () => {
+  it.each([
+    [60, 'fasting', '00:01:00'],
+    [62, 'fasting', '00:01:02'],
+    [90, 'fasting', '00:01:30'],
+  ])('show correct time for each', (value, mode, expectedTime) => {
     render(
       <Provider store={store}>
-        <Countdown value={60} mode="fasting" />
+        <Countdown value={value} mode={mode} />
       </Provider>,
     );
 
     let remainingText = screen.getByText(/:/);
-    expect(remainingText.props.children).toBe('00:01:00');
+    expect(remainingText.props.children).toBe(expectedTime);
   });
 });
