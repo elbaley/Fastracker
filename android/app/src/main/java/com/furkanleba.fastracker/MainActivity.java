df7611bce4663db846df4366191cc5486e1ddb33
elbaley <lebafurkan@outlook.com>
2023-07-15 04:09:55 +0300

    add firebase

--- android/app/src/main/java/com/furkanleba.fastracker/MainActivity.java
+++ android/app/src/main/java/com/furkanleba.fastracker/MainActivity.java
@@ -1,4 +1,4 @@
-package com.fastracker;
+package com.furkanleba.fastracker;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/furkanleba.fastracker/MainApplication.java
+++ android/app/src/main/java/com/furkanleba.fastracker/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fastracker;
+package com.furkanleba.fastracker;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
